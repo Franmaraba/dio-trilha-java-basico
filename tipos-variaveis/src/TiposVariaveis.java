af10bd8e1484f5945428935798ab0a6ee2632211
Francisco <marabas.sp@gmail.com>
2024-05-25 15:02:26 -0300

    finalizando tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //ESTUDAR
+        //tipos primitivos
+        //classes string que reperesenta texto na aplicação
+
+        String meuNome = "Francisco Rodrigues"
+        
+        double salarioMinimo = 2.500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
